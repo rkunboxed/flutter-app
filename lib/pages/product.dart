1901cec267295f00e5524b459c455b4f879bf166
Rona Kilmer <rkunboxed@gmail.com>
2018-08-24 10:34:39 -0600

    added alert dialog example

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -8,39 +8,68 @@ class ProductPage extends StatelessWidget {
 
   ProductPage(this.title, this.imageUrl);
 
-  @override
-    Widget build(BuildContext context) {
-      return WillPopScope(onWillPop: () {//willpopscope is manual way to handle back button
-        print('back button pressed');
-        Navigator.pop(context, false);//this allows data to be passed (in this case, `false`)
-        return Future.value(false); //this allows the actual routing; false says ignore original request and proceed with the manual one (the navigator pop above)
-      }, child:
-        Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Center(child: Column(
-          // wrapping the column in a center widget plus the following two settings
-          // will center everything vertically and horizontally
-          //mainAxisAlignment: MainAxisAlignment.center, //(vertical entering)
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(imageUrl),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: Text(title),
+  _showWarningDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Are you sure?'),
+          content:
+              Text('This action cannot be undone'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('NEVERMIND'),
+              onPressed: () {
+                Navigator.pop(context); //this just closes the dialog
+              },
             ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                // child: Text('Custom Back Btn'),
-                // onPressed: () => Navigator.pop(context)
-                child: Text('DELETE'),
-                onPressed: () => Navigator.pop(context, true)
-              )
-            )
-          ]),
-      )),);
-    }
+            FlatButton(
+              child: Text('YES DELETE'),
+              onPressed: () {
+                Navigator.pop(context); //closes dialog
+                Navigator.pop(context,true); //continues on with `true` value that deletes the product
+              },
+            ),
+          ],
+        );
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () {
+        //willpopscope is manual way to handle back button
+        print('back button pressed');
+        Navigator.pop(context,
+            false); //this allows data to be passed (in this case, `false`)
+        return Future.value(
+            false); //this allows the actual routing; false says ignore original request and proceed with the manual one (the navigator pop above)
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: Center(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(imageUrl),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(title),
+                  ),
+                  Container(
+                      padding: EdgeInsets.all(10.0),
+                      child: RaisedButton(
+                          color: Theme.of(context).accentColor,
+                          child: Text('DELETE'),
+                          //onPressed: () => Navigator.pop(context, true)
+                          onPressed: () => _showWarningDialog(context)
+                      )
+                  )
+                ]),
+          )),
+    );
+  }
 }
